0e5b447f966154ad116fd3511b4ab713e7340cfc
Suhail Ali <mohammedsuhailali3@gmail.com>
2022-05-23 21:07:03 +0200

    Models

---
+++ Models/Properties/Property.java
@@ -0,0 +1,32 @@
+package Models.Properties;
+
+public class Property {
+
+    private String name;
+    private String owner;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
